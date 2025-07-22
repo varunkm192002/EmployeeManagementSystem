01d72a7561f7e872834688684ec39dc64942801e
varun <varunkm192002@gmail.com>
2025-07-22 22:59:49 +0530

    completed Admin Dashboard with navigation bar and sidebar

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { useAuth } from '../context/authContext'
+import AdminSidebar from '../components/dashboard/AdminSidebar'
+import Navbar from '../components/dashboard/Navbar'
+import AdminSummary from '../components/dashboard/AdminSummary'
+import { Outlet } from 'react-router-dom'
 
 const AdminDashboard = () => {
     const { user } = useAuth()
 
     return (
-        <div>
-            AdminDashboard {user && user.name}
+        <div className='flex'>
+            <AdminSidebar />
+            <div className='flex-1 ml-64 bg-grey-100 h-screen'>
+                <Navbar />
+                <Outlet />
+            </div>
+
         </div>
     )
 }
